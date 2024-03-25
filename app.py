a1854a0651756164117e110abf8f3a053beb3f40
Prajwal <praj3009wal@gmail.com>
2024-03-25 08:35:23 +0530

    firsttime

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from transformers import BitsAndBytesConfig, GemmaTokenizer
+from transformers import BitsAndBytesConfig
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-HF_TOKEN = os.getenv("HF_TOKEN")
 
 model_id = "Prajwal3009/gemma2bunisys"
 bnb_config = BitsAndBytesConfig(
@@ -15,20 +13,20 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_quant_type="nf4",
     bnb_4bit_compute_dtype=torch.bfloat16
 )
-tokenizer = AutoTokenizer.from_pretrained(model_id, token=HF_TOKEN)
+
+# Load tokenizer and model on CPU
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False)
 model = AutoModelForCausalLM.from_pretrained(model_id,
                                              quantization_config=bnb_config,
-                                             device_map={"":0},
-                                             token=HF_TOKEN)
-
+                                             device="cpu")
 
 @st.cache(allow_output_mutation=True)
 def generate_response(user_input):
     inputs = tokenizer(user_input, return_tensors="pt")
-    outputs = model.generate(**inputs, max_length=1000)
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_length=1000)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return response
-
 
 st.title("Gemma Chatbot")
 st.write("Welcome to Gemma Chatbot! Ask me anything about Unisys.")
@@ -40,4 +38,3 @@ if st.button("Ask"):
     else:
         response = generate_response(user_input)
         st.text_area("Gemma:", value=response, height=200, max_chars=None, key=None)
-
